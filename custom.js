f6e85e6b87ae00030cde08cf2c54cdff0303f38c
shresthaemma <50352907+shresthaemma@users.noreply.github.com>
2019-07-30 11:35:22 +0545

    javascript1

---
+++ custom.js
@@ -0,0 +1,21 @@
+//alert("hello");
+var a=10;
+var b=5;
+c=a+b;
+document.write("<br> The sum of "+a+" and "+b+" is "+c );
+//to check odd or even
+/*prompt("message")
+let num= prompt("Enter the number: ");
+//var num =20;
+if(num%2==0){
+    document.write("<br> "+num+" is even.");
+}
+else{
+    document.write("<br> "+num+" is odd.");
+}
+*/
+prompt("message")
+let num1= parseInt(prompt("Enter the number1: "));
+let num2= parseInt(prompt("Enter the number2: "));
+c=num1+num2;
+document.write("<br> The sum of "+num1+" and "+num2+" is "+c );
